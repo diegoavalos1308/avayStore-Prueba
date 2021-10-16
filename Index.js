cd976f2ad1e4cce66f6643ea6203ff757d783156
diegoavalos1308 <92642645+diegoavalos1308@users.noreply.github.com>
2021-10-16 10:37:24 -0600

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,58 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
+
+const getRemainingTime = deadline => {
+    let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays = Math.floor(remainTime / (3600 * 24));
+  
+    return {
+      remainSeconds,
+      remainMinutes,
+      remainHours,
+      remainDays,
+      remainTime
+    }
+  };
+  
+  const countdown = (deadline,elem,finalMessage) => {
+    const el = document.getElementById(elem);
+  
+    const timerUpdate = setInterval( () => {
+      let t = getRemainingTime(deadline);
+      el.innerHTML = `${t.remainDays}: ${t.remainHours}: ${t.remainMinutes}: ${t.remainSeconds}`;
+  
+      if(t.remainTime <= 1) {
+        clearInterval(timerUpdate);
+        el.innerHTML = finalMessage;
+      }
+  
+    }, 1000)
+  };
+  
+  countdown('jan 01 2022 24:00:00 GMT-0500', 'clock', '¡Ya empezó!');
